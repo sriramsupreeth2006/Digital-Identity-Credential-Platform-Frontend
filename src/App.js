f877557c55e7ddee1d60a6e95c10befad99b1d96
sriramsupreeth2006 <sriram.supreethd@gmail.com>
2025-11-04 09:09:41 +0530

    Added interface for Help Center and activated the button in the footer

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./Home";
 import Register from "./Register";
 import Login from "./Login";
 import ResetPassword from "./ResetPassword";
-
+import HelpCenter from "./HelpCenter"
 function App() {
   return (
     <Router>
@@ -13,6 +13,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/home" element={<h1>Home Page</h1>} />
+        <Route path="/help" element={<HelpCenter />} />
       </Routes>
     </Router>
   );
